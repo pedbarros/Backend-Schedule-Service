aa554eb8cdf1452a93152e9e09185b312809f9e8
Pedro Barros <pedro.barros@herospark.com>
2020-04-10 13:46:08 -0300

    adicionando paginaçao no agendamento

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -14,11 +14,8 @@ export default async (req, res, next) => {
   const [, token] = authHeader.split(' ');
 
   try {
-
-    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);    
     req.userId = decoded.id;
-
     return next();
   } catch (error) {
     return res.status(401).json({ error: 'Token invalid' });
