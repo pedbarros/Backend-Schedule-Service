112c5bebc7af76fa1a7c1b2b5522ca3604ba211f
Pedro Barros <pedro.barros@herospark.com>
2020-04-23 18:50:22 -0300

    Utilizando Rate Limit com redis

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
+import * as Sentry from '@sentry/node';
+import cors from 'cors';
 import 'dotenv/config';
-
 import express from 'express';
-import routes from './routes';
+import 'express-async-errors';
+import helmet from 'helmet';
 import path from 'path';
 import Youch from 'youch';
-import * as Sentry from '@sentry/node';
 import sentryConfig from './config/sentry';
-
-import 'express-async-errors';
 import './database';
+import routes from './routes';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
+
 
 class App {
   constructor() {
@@ -23,8 +27,23 @@ class App {
 
   middlewares () {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(helmet());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(new RateLimit({
+        store: new RateLimitRedis({
+          client: redis.createClient({
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT
+          })
+        }),
+        windowMs: 1000 * 60 * 15,
+        max: 10,
+      }));
+    }
   }
 
   routes () {
